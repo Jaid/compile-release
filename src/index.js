2ffa5ce5ff61bf94cd2e4939b1b22f4808c7ce14
Jaid <jaid.jsx@gmail.com>
2019-05-27 00:39:06 +0200

    Fixed dpkg-deb call
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -133,7 +133,7 @@ const job = async argv => {
         |> Object.entries
         |> #.map(([key, value]) => `${key}: ${value}`)
         |> #.join("\n")
-        await fsp.outputFile(path.join(debFolder, "debian", "control"), `${debInfo}\n`, "utf8")
+        await fsp.outputFile(path.join(debFolder, "DEBIAN", "control"), `${debInfo}\n`, "utf8")
         await execa(dpkgDebFile, ["--build", debFolder, releaseFolder])
       }
       compileExecutableTasks.push(buildDebTask)
