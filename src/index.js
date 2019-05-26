886741cac4aacc313a95b5b3fe3d3f69f54d9c65
Jaid <jaid.jsx@gmail.com>
2019-05-26 18:58:52 +0200

    Implemented deb building
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,21 +8,32 @@ import globby from "globby"
 import archiver from "archiver"
 import {exec} from "pkg"
 import promiseSequence from "promise-sequence"
+import whichPromise from "which-promise"
+import execa from "execa"
+import jaidLogger from "jaid-logger"
 
-import {log} from "./logger"
+const logger = jaidLogger(_PKG_TITLE)
+
+const findDpkgDebFile = async () => {
+  try {
+    return await whichPromise("dpkg-deb")
+  } catch {
+    return false
+  }
+}
 
 const job = async argv => {
   const packageFolder = process.cwd()
   const buildFolder = path.join(packageFolder, "dist", "package", "production")
   const buildFolderExists = await fsp.pathExists(buildFolder)
   if (!buildFolderExists) {
-    log(`Build folder ${buildFolder} not found`)
+    logger.warn(`Build folder ${buildFolder} not found`)
     process.exit(1)
   }
   const packageJsonFile = path.join(buildFolder, "package.json")
   const packageJsonFileExists = await fsp.pathExists(packageJsonFile)
   if (!packageJsonFileExists) {
-    log(`${packageJsonFile} not found`)
+    logger.warn(`${packageJsonFile} not found`)
     process.exit(1)
   }
   const pkg = await fsp.readJson(packageJsonFile)
@@ -31,11 +42,11 @@ const job = async argv => {
     for (const knownMode of ["index", "cli", "app"]) {
       const exists = await fsp.pathExists(path.join(buildFolder, `${knownMode}.js`))
       if (exists) {
-        log("Mode: %s", knownMode)
+        logger.info("Mode: %s", knownMode)
         return knownMode
       }
     }
-    log("No mode determined")
+    logger.warn("No mode determined")
   }
   const mode = await getMode()
   const releaseFolder = path.join(packageFolder, "dist", "github")
@@ -96,8 +107,38 @@ const job = async argv => {
         file: `${packageId}_alpine_x64`,
       },
     ]
-    const createExecutables = promiseSequence(targets.map(({id, file}) => () => exec([...pkgOptions, "--target", id, "--output", path.join(releaseFolder, file)])))
-    releaseTasks.push(createExecutables)
+    const compileExecutableTasks = targets.map(({id, file}) => () => exec([...pkgOptions, "--target", id, "--output", path.join(releaseFolder, file)]))
+    const dpkgDebFile = await findDpkgDebFile()
+    if (dpkgDebFile) {
+      const buildDebTask = async () => {
+        const scriptBinaryFile = `${packageId}_linux_x64`
+        const debFolder = path.join(packageFolder, "dist", "deb")
+        const debBinFolder = path.join(debFolder, "usr", "local", "bin")
+        await fsp.ensureDir(debBinFolder)
+        const debBinFile = path.join(debBinFolder, pkg.bin || pkg.name)
+        await fsp.copyFile(path.join(releaseFolder, scriptBinaryFile), path.join(releaseFolder, debBinFile))
+        const {size} = await fsp.stat(debBinFile)
+        const debInfo = {
+          Package: pkg.name,
+          Version: pkg.version,
+          Maintainer: pkg.author,
+          Description: pkg.description,
+          "Installed-Size": size,
+          Section: "base",
+          Priority: "optional",
+          Architecture: "amd64",
+        }
+        |> Object.entries
+        |> #.map(([key, value]) => `${key}: ${value}`)
+        |> #.join("\n")
+        fsp.outputFile(path.join(debFolder, "DEBIAN", "control"), debInfo, "utf8")
+      }
+      compileExecutableTasks.push(buildDebTask)
+      await execa(dpkgDebFile, ["--build", debFolder, releaseFolder])
+    } else {
+      logger.warn("Skipping deb building, dpkg-deb not found")
+    }
+    releaseTasks.push(compileExecutableTasks |> promiseSequence)
   }
   await Promise.all(releaseTasks)
 }
