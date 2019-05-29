8bcc52c4e4c29de5934f71a4733d4bd91509fcec
Jaid <jaid.jsx@gmail.com>
2019-05-29 23:07:56 +0200

    Added 4 new compiled builds for debugging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import whichPromise from "which-promise"
 import execa from "execa"
 import jaidLogger from "jaid-logger"
 import stringifyAuthor from "stringify-author"
+import moment from "moment"
 
 const logger = jaidLogger(_PKG_TITLE)
 
@@ -50,6 +51,9 @@ const job = async argv => {
     logger.warn("No mode determined")
   }
   const mode = await getMode()
+  const prodScript = path.join(buildFolder, `${mode}.js`)
+  const devScript = path.join(packageFolder, "dist", "package", "development", `${mode}.js`)
+  const hasDevelopmentBuild = await fsp.pathExists(devScript)
   const releaseFolder = path.join(packageFolder, "dist", "github")
   const [copyList, miniArchiveList] = await Promise.all([
     globby(["readme.*", "*.d.ts", "package.json", "license.*", "thirdPartyLicenses.*"], {
@@ -83,32 +87,60 @@ const job = async argv => {
   releaseTasks.push(miniArchive.finalize())
   if (mode === "cli" || mode === "app") {
     const pkgOptions = [
-      path.join(buildFolder, `${mode}.js`),
       "--config",
       path.join(buildFolder, "package.json"),
       "--options",
       "max_old_space_size=4000",
       "--public",
     ]
-    const targets = [
+    let targets = [
       {
         id: "latest-win-x64",
-        file: `${packageId}_windows_x64`,
+        input: prodScript,
+        output: `${packageId}_windows_x64`,
       },
       {
         id: "latest-macos-x64",
-        file: `${packageId}_mac_x64`,
+        input: prodScript,
+        output: `${packageId}_mac_x64`,
       },
       {
         id: "latest-linux-x64",
-        file: `${packageId}_linux_x64`,
+        input: prodScript,
+        output: `${packageId}_linux_x64`,
       },
       {
         id: "latest-alpine-x64",
-        file: `${packageId}_alpine_x64`,
+        input: prodScript,
+        output: `${packageId}_alpine_x64`,
       },
     ]
-    const compileExecutableTasks = targets.map(({id, file}) => () => exec([...pkgOptions, "--target", id, "--output", path.join(releaseFolder, file)]))
+    if (hasDevelopmentBuild) {
+      targets = [
+        {
+          id: "latest-win-x64",
+          input: devScript,
+          output: `${packageId}_debug_windows_x64`,
+        },
+        {
+          id: "latest-macos-x64",
+          input: devScript,
+          output: `${packageId}_debug_mac_x64`,
+        },
+        {
+          id: "latest-linux-x64",
+          input: devScript,
+          output: `${packageId}_debug_linux_x64`,
+        },
+        {
+          id: "latest-alpine-x64",
+          input: devScript,
+          output: `${packageId}_debug_alpine_x64`,
+        },
+        ...targets,
+      ]
+    }
+    const compileExecutableTasks = targets.map(target => () => exec([...pkgOptions, "--target", target.id, "--output", path.join(releaseFolder, target.output), target.input]))
     releaseTasks.push(compileExecutableTasks |> promiseSequence)
   }
   await Promise.all(releaseTasks)
@@ -135,6 +167,10 @@ const job = async argv => {
         Section: "base",
         Priority: "optional",
         Architecture: "amd64",
+        Date: moment().format("ddd, DD MMM YYYY HH:mm:ss ZZ"),
+      }
+      if (pkg.homepage) {
+        debInfo.Homepage = pkg.homepage
       }
       const controlContent = debInfo
         |> Object.entries
